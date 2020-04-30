3fc1198fbc89b7bc04d15e2c61a3743279cd48c6
Joseph Kao <josephtkao@gmail.com>
2020-04-30 05:39:34 +0000

    changed scraper class

--- lib/cli_project/scraper.rb
+++ lib/cli_project/scraper.rb
@@ -3,47 +3,32 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-
-  def self.get_page(url)
+  def get_page(url)
     Nokogiri::HTML(open(url))
   end
 
-  def self.scrape_index_page(index_url)
-    doc = self.get_page(index_url)
-    final = []
-    students = doc.css(".student-card")
-    students.each do |card|
-      name = card.css("a .student-name").text
-      scraped_student = {:name => name,
-      :location => card.css("a .student-location").text,
-      :profile_url => card.css("a").attribute("href").text}
-      final << scraped_student
-    end
-    final
+  def scrape_index
+    doc = self.get_page(@index_url)
+    cities = []
+    list = doc.css(".col-wrapper-357f4 li a")
+    list.each{|x| cities << x.text}
+    print cities
+    return cities
   end
 
-  def self.scrape_profile_page(profile_url)
-    doc = self.get_page(profile_url)
-    ind_site = doc.css(".social-icon-container a")
-    links = {}
-    ind_site.each do |site|
-      if site.values.first.include?("twitter")
-        links[:twitter] = site.values[0]
-      elsif site.values.first.include?("linkedin")
-        links[:linkedin] = site.values[0]
-      elsif site.values.first.include?("github")
-        links[:github] = site.values[0]
-      else
-        links[:blog] = site.values[0]
-      end
-    end
-    links[:profile_quote] = doc.css(".profile-quote").text
-    links[:bio] = doc.css(".description-holder p").text
-    links
-
+  def scrape_city(city_url)
+    doc = self.get_page(city_url)
+    games = doc.css(".content_box")
+    print games
   end
+
+  def initialize
+    @index_url = "https://pickupultimate.com/cities"
+  end
+
 
 end
 
-
-Scraper.scrape_profile_page("https://learn-co-curriculum.github.io/student-scraper-test-page/students/eric-chu.html")
+city_url = "https://pickupultimate.com/map/city/annarbor"
+scraper = Scraper.new
+scraper.scrape_index
