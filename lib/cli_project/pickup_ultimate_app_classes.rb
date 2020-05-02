eca4baf89c704664e8af18422c4215687961b064
Joseph Kao <josephtkao@gmail.com>
2020-05-02 04:22:05 +0000

    updated classes

--- lib/cli_project/pickup_ultimate_app_classes.rb
+++ lib/cli_project/pickup_ultimate_app_classes.rb
@@ -23,12 +23,11 @@ class City
 end
 
 class Game
-  attr_accessor :name, :contact, :city, :description
+  attr_accessor :name, :city, :description
   @@all = []
 
   def initialize(name, city)
     @name = name
-    @contact = contact
     @city = city.name
     @@all << self
   end
@@ -36,22 +35,25 @@ class Game
   def self.add_from_Nokoguri(obj, city)
     scraper = Scraper.new
     names = []
-    obj = scraper.scrape_city("https://pickupultimate.com/map/city/annarbor")
+    obj = scraper.scrape_city("https://pickupultimate.com/map/city/atlanta")
     obj.each do |game|
       name = game.css("h1").text
       new_game = Game.new(name, city) unless name.include?("Ultimate Frisbee Games in ")
-      names << name
+      names << name unless name.include?("Ultimate Frisbee Games in ")
     end
     counter = 0
+    contact_string = obj.text
+    # print contact_string
     while counter < names.length
-      contact = obj.text.split("Contact: ")[1].split(/\s/)
-      # puts "#{Game.all[counter].name}"
-      # puts "#{names[counter]}"
-      puts "#{names}"
-      # puts "#{Game.all.select{|x| x.name == names[counter]}}"
-      counter += 1
+        str_process = contact_string.split("Description: ")
+        description = str_process[1].split("view game")[0]
+        str_process[2..-1].each{|x| x.prepend("Description: ")}
+        contact_string = str_process[2..-1].join(" ")
+        Game.all.select{|x| x.name == names[counter]}[0].description = description
+        counter += 1
     end
   end
+
 
   def self.all
     @@all
@@ -61,4 +63,4 @@ class Game
 end
 annarbor = City.new("Ann Arbor")
 Game.add_from_Nokoguri("",annarbor)
-# print Game.all
+print Game.all
